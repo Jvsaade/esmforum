b66c0cd7175413c17bc3e7feb237365546c8272e
Jvsaade <jvssimao@gmail.com>
2024-06-28 22:16:09 -0300

    Mudando para get_pergunta

--- testes/modelo.test.js
+++ testes/modelo.test.js
@@ -37,7 +37,6 @@ test('Testando resposta a perguntas', () => {
   expect(modelo.get_num_respostas(0)).toBe(1);
   expect(modelo.get_num_respostas(1)).toBe(2);
   expect(modelo.get_num_respostas(2)).toBe(3);
-  const pergunta1 = modelo.get_respostas(0);
+  const pergunta1 = modelo.get_pergunta(0);
   expect(pergunta1.id_pergunta).toBe(0);
-
 });
